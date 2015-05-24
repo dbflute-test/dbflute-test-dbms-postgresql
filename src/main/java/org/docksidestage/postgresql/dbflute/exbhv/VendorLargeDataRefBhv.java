4bd5f81c030f01d7c59b7c2da4ba8fb3fea86ec5
jflute <dbflute@gmail.com>
2015-05-24 16:21:16 +0900

    Release DBFlute-1.1.0-sp3

--- src/main/java/org/docksidestage/postgresql/dbflute/exbhv/VendorLargeDataRefBhv.java
+++ src/main/java/org/docksidestage/postgresql/dbflute/exbhv/VendorLargeDataRefBhv.java
@@ -81,9 +81,9 @@ public class VendorLargeDataRefBhv extends BsVendorLargeDataRefBhv {
                         return new TnBasicSelectHandler(_dataSource, executedSql, _resultSetHandler, _statementFactory) {
                             @Override
                             protected void doLogSql(Object[] args, Class<?>[] argTypes, boolean logEnabled, boolean sqlFireHook,
-                                    boolean hasSqlLog, boolean hasSqlResult, Object sqlLogRegistry) {
+                                    boolean hasSqlLog, boolean hasSqlResult) {
                                 _loggingMarkMap.put("doLogSql", logEnabled + ", " + sqlFireHook + ", " + hasSqlLog + ", " + hasSqlResult);
-                                super.doLogSql(args, argTypes, logEnabled, sqlFireHook, hasSqlLog, hasSqlResult, sqlLogRegistry);
+                                super.doLogSql(args, argTypes, logEnabled, sqlFireHook, hasSqlLog, hasSqlResult);
                             };
 
                             @Override
@@ -134,9 +134,9 @@ public class VendorLargeDataRefBhv extends BsVendorLargeDataRefBhv {
                         return new TnBatchUpdateHandler(_dataSource, _statementFactory, sql, _beanMetaData, boundPropTypes) {
                             @Override
                             protected void doLogSql(Object[] args, Class<?>[] argTypes, boolean logEnabled, boolean sqlFireHook,
-                                    boolean hasSqlLog, boolean hasSqlResult, Object sqlLogRegistry) {
+                                    boolean hasSqlLog, boolean hasSqlResult) {
                                 _loggingMarkMap.put("doLogSql", logEnabled + ", " + sqlFireHook + ", " + hasSqlLog + ", " + hasSqlResult);
-                                super.doLogSql(args, argTypes, logEnabled, sqlFireHook, hasSqlLog, hasSqlResult, sqlLogRegistry);
+                                super.doLogSql(args, argTypes, logEnabled, sqlFireHook, hasSqlLog, hasSqlResult);
                             };
 
                             @Override
